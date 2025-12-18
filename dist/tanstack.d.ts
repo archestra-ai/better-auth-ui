d2ca7c6efcdf9ebc4215add001817ee4795b547a
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2025-12-18 17:41:57 +0100

    build

---
+++ dist/tanstack.d.ts
@@ -0,0 +1,18 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+import { m as AuthUIProviderProps } from './auth-ui-provider-CyuE3MPo.js';
+import 'react';
+import './view-paths-BQ1wPAI8.js';
+import './auth-mutators-D6qMYRC1.js';
+import '@better-auth/passkey';
+import '@better-fetch/fetch';
+import 'better-auth';
+import 'better-auth/plugins/organization';
+import 'better-auth/react';
+import 'better-auth/client/plugins';
+import '@better-auth/passkey/client';
+import 'better-auth/plugins';
+import 'better-auth/social-providers';
+
+declare function AuthUIProviderTanstack({ children, authClient, hooks: hooksProp, mutators: mutatorsProp, onSessionChange: onSessionChangeProp, ...props }: AuthUIProviderProps): react_jsx_runtime.JSX.Element;
+
+export { AuthUIProviderTanstack };
